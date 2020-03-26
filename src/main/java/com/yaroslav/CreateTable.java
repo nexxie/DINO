cbd10979d3ea916e7d6ce1abab7fd9b870b2ddb2
nexxie <nexxie1@gmail.com>
2020-03-26 16:50:55 +0300

    Fix CreateTable class

--- src/main/java/com/yaroslav/CreateTable.java
+++ src/main/java/com/yaroslav/CreateTable.java
@@ -1,14 +1,163 @@
 package com.yaroslav;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import javax.xml.transform.Result;
+import java.sql.*;
 import java.util.Properties;
 
 public class CreateTable {
 
-  static final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/";
-  static final String USER = "nexxie";
-  static final String PASS = "Z57105930";
+
+  static String DB_URL_FOR_CREATE_DB = "jdbc:postgresql://127.0.0.1:5432/postgres";
+  static String DB_URL = "jdbc:postgresql://127.0.0.1:5432/traffic_limits";
+  static String USER = "nexxie";
+  static String PASS = "Z57105930";
+
+
+  public static Connection getDBConnectionForNewDB() {
+    Connection dbConnection = null;
+    try {
+      Class.forName("com.postgresql.Driver");
+    } catch (ClassNotFoundException e) {
+      System.out.println(e.getMessage());
+    }
+    try {
+      dbConnection = DriverManager.getConnection(DB_URL_FOR_CREATE_DB, USER, PASS);
+      return dbConnection;
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+    }
+    return dbConnection;
+  }
+
+  public static Connection getDBConnection() {
+    Connection dbConnection = null;
+    try {
+      Class.forName("com.postgresql.Driver");
+    } catch (ClassNotFoundException e) {
+      System.out.println(e.getMessage());
+    }
+    try {
+      dbConnection = DriverManager.getConnection(DB_URL, USER, PASS);
+      return dbConnection;
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+    }
+    return dbConnection;
+  }
+
+  public static void createTable() throws SQLException {
+    Connection dbConnection = null;
+    Statement statement = null;
+
+
+    String createTableSQL = "CREATE TABLE limits_per_hour("
+            + "limit_name text, "
+            + "limit_value INTEGER NOT NULL, "
+            + "effective_date DATE "
+            + ")";
+
+    try {
+      dbConnection = getDBConnection();
+      statement = dbConnection.createStatement();
+
+      // выполнить SQL запрос
+      statement.execute(createTableSQL);
+      System.out.println("Table  is created!");
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+    } finally {
+      if (statement != null) {
+        statement.close();
+      }
+      if (dbConnection != null) {
+        dbConnection.close();
+      }
+    }
+  }
+
+
+  public static void createDB() throws SQLException {
+    Connection dbConnection = null;
+    Statement statement = null;
+
+    String createDBSQL = "CREATE DATABASE traffic_limits";
+
+    try {
+      dbConnection = getDBConnectionForNewDB();
+      statement = dbConnection.createStatement();
+
+      // выполнить SQL запрос
+      statement.execute(createDBSQL);
+      System.out.println("DB  is created!");
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+    } finally {
+      if (statement != null) {
+        statement.close();
+      }
+      if (dbConnection != null) {
+        dbConnection.close();
+      }
+    }
+
 
   }
 
+  static void insertTable() throws SQLException {
+    Connection dbConnection = null;
+    Statement statement = null;
+
+    String insertTableSQL = "INSERT INTO limits_per_hour"
+            + "(limit_name, limit_value,effective_date) " + "VALUES"
+            + "('min',1234, CURRENT_DATE)";
+
+    try {
+      dbConnection = getDBConnection();
+      statement = dbConnection.createStatement();
+
+      // выполнить SQL запрос
+      statement.execute(insertTableSQL);
+      System.out.println("Data has been inserted !");
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+    } finally {
+      if (statement != null) {
+        statement.close();
+      }
+      if (dbConnection != null) {
+        dbConnection.close();
+      }
+    }
+
+
+  }
+
+  static void selectData() throws SQLException {
+    Connection dbConnection = null;
+    Statement statement = null;
+    ResultSet resultSet = null;
+
+    String selectDataSql = "SELECT * FROM  limits_per_hour"
+            + "(limit_name, limit_value,effective_date) " + "VALUES"
+            + "('test',1234, CURRENT_DATE)";
+
+    try {
+      dbConnection = getDBConnection();
+      statement = dbConnection.createStatement();
+
+      statement.execute(selectDataSql);
+
+      System.out.println("Data has been inserted !");
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+    } finally {
+      if (statement != null) {
+        statement.close();
+      }
+      if (dbConnection != null) {
+        dbConnection.close();
+      }
+    }
+
+  }
+}
--- src/main/java/com/yaroslav/Main.java
+++ src/main/java/com/yaroslav/Main.java
@@ -12,22 +12,55 @@ import java.net.PasswordAuthentication;
 import java.sql.*;
 import java.util.Properties;
 
+
 public class Main {
 
-  private static String DB_URL = "jdbc:postgresql://127.0.0.1:5432/traffic_limits";
-  private static String USER = "nexxie";
-  private static String PASS = "Z57105930";
 
-
-
-  public static void main(String[] args) throws SQLException, ClassNotFoundException {
+  public static void main(String[] args) throws SQLException, ClassNotFoundException, InterruptedException {
     Class.forName("org.postgresql.Driver");
-    getDBConnection();
-   // createDB();
-   // createDbUserTable();
-    insertTable();
 
-    ////////////////////////////////////
+    Runnable createDB = new Runnable() {
+      public void run() {
+        CreateTable createTable = new CreateTable();
+
+        createTable.getDBConnectionForNewDB();
+        try {
+          createTable.createDB();
+        } catch (SQLException e) {
+          e.printStackTrace();
+        }
+
+      }
+
+    };
+
+    Runnable createTable = new Runnable() {
+      public void run() {
+        CreateTable createTable = new CreateTable();
+        createTable.getDBConnection();
+        try {
+          createTable.createTable();
+        } catch (SQLException e) {
+          e.printStackTrace();
+        }
+        try {
+          createTable.insertTable();
+        } catch (SQLException e) {
+          e.printStackTrace();
+        }
+      }
+    };
+
+
+    Thread thread = new Thread(createDB);
+    thread.start();
+    Thread thread1 = new Thread(createTable);
+    thread1.start();
+
+
+  }
+}
+  ////////////////////////////////////
 
 //    SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("Spark");
 //    JavaSparkContext context = new JavaSparkContext(conf);
@@ -39,113 +72,4 @@ public class Main {
 //
 //    System.out.println();
 
-  }
 
-
-  private static Connection getDBConnection() {
-    Connection dbConnection = null;
-    try {
-      Class.forName("com.postgresql.Driver");
-    } catch (ClassNotFoundException e) {
-      System.out.println(e.getMessage());
-    }
-    try {
-      dbConnection = DriverManager.getConnection(DB_URL, USER, PASS);
-      return dbConnection;
-    } catch (SQLException e) {
-      System.out.println(e.getMessage());
-    }
-    return dbConnection;
-  }
-
-  private static void createDbUserTable() throws SQLException {
-    Connection dbConnection = null;
-    Statement statement = null;
-
-
-    String createTableSQL = "CREATE TABLE limits_per_hour("
-            + "limit_name text, "
-            + "limit_value INTEGER NOT NULL, "
-            + "effective_date DATE "
-            + ")";
-
-    try {
-      dbConnection = getDBConnection();
-      statement = dbConnection.createStatement();
-
-      // выполнить SQL запрос
-      statement.execute(createTableSQL);
-      System.out.println("Table  is created!");
-    } catch (SQLException e) {
-      System.out.println(e.getMessage());
-    } finally {
-      if (statement != null) {
-        statement.close();
-      }
-      if (dbConnection != null) {
-        dbConnection.close();
-      }
-    }
-  }
-
-  private static void createDB() throws SQLException {
-    Connection dbConnection = null;
-    Statement statement = null;
-
-    String createDBSQL = "CREATE DATABASE traffic_limits";
-
-    try {
-      dbConnection = getDBConnection();
-      statement = dbConnection.createStatement();
-
-      // выполнить SQL запрос
-      statement.execute(createDBSQL);
-      System.out.println("Table  is created!");
-    } catch (SQLException e) {
-      System.out.println(e.getMessage());
-    } finally {
-      if (statement != null) {
-        statement.close();
-      }
-      if (dbConnection != null) {
-        dbConnection.close();
-      }
-    }
-
-
-  }
-
-  private static void insertTable() throws SQLException {
-    Connection dbConnection = null;
-    Statement statement = null;
-
-    String insertTableSQL = "INSERT INTO limits_per_hour"
-            + "(limit_name, limit_value,effective_date) " + "VALUES"
-            + "('test',1234, CURRENT_DATE)"
-            ;
-
-    try {
-      dbConnection = getDBConnection();
-      statement = dbConnection.createStatement();
-
-      // выполнить SQL запрос
-      statement.execute(insertTableSQL);
-      System.out.println("Table  is created!");
-    } catch (SQLException e) {
-      System.out.println(e.getMessage());
-    } finally {
-      if (statement != null) {
-        statement.close();
-      }
-      if (dbConnection != null) {
-        dbConnection.close();
-      }
-    }
-
-
-  }
-
-
-
-
-}
